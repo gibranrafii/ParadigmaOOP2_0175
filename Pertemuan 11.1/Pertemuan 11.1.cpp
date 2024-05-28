478f0c7818a1138258bb46ea1b3692fe3c755512
gibranrafii <gibranrafi21@gmail.com>
2024-05-28 10:17:00 +0700

    Membuat class seseorang dengan menggunakan polymorphy virtual

--- Pertemuan 11.1/Pertemuan 11.1.cpp
+++ Pertemuan 11.1/Pertemuan 11.1.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+    // pure virtual function
+    virtual void pesan() = 0;
+
+    // virtual function biasa
+    //virtual void pesan() 
+    //{
+    //    cout<<"Pesan dari seseorang"<<endl;
+    //}
+};
+
 
 int main()
 {
